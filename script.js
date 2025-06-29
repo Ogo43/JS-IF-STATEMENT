8f1e85481636c9fa798237cc2d4c6736f7410e14
Ogochukwu Emeh <emehaogochukwu@gmail.com>
2025-06-29 11:35:45 -0700

    Create JavaScript IF Statement

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ document.addEventListener("DOMContentLoaded", function() {
            event.preventDefault();
            
             const ageInput = prompt("Enter your age");
-            const age = Number(age);
+            const age = Number(ageInput);
 
-           if (!age || isNaN(ageInput)) {
+           if (!age || isNaN(age)) {
              alert("Please enter a valid age");
            } else if(age >= 18) {
-            window.location.href = "./adult.html";
-           } else {
+              window.location.href = "./adult.html";
+          } else {
             window.location.href = "./kids.html";
            }
         })
